12225750af7176d6d1e8331b3d53aba2a17f5479
JJones <jjones@brysontech.com>
2018-01-04 22:21:24 -0500

    Working on the login form. Created a "login" route to make the login page look like my baseline template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,12 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        return $this->render('Login/index.html.twig');
+    }
 }
